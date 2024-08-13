9b5c0b444a959efb6917ab9b41fec5f0c0a089d2
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-13 14:40:16 -0400

    completed favorite school subject exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          Console.WriteLine("What's your favorite subject at school?");
+          string favoriteSubject = Console.ReadLine();
+          
+          switch (favoriteSubject)
+          {
+              case "Math":
+                Console.WriteLine("Math is cool because it's all about solving problems!");
+                break;
+                
+              case "English":
+                  Console.WriteLine("English is awesome, I love to write!");
+                  break;
+              
+              case "History":
+                  Console.WriteLine("History is so interesting. I love to learn about Ancient Rome!");
+                  break;
+              
+              case "Science":
+                  Console.WriteLine("Like Bill Nye said... science rules!");
+                  break;
+              
+              default:
+                Console.WriteLine("Interesting choice! School is cool!");
+                break;
+
+          }
+
         }
     }
 }
